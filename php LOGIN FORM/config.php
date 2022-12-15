0031905696460eb89dce9903d09140b67cfcf694
Jaya balaji V <111067200+jayabalaji2002@users.noreply.github.com>
2022-12-15 15:20:03 +0530

    Delete php LOGIN FORM directory

--- php LOGIN FORM/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-
-    $databaseURL="https://signupform-a0d43-default-rtdb.firebaseio.com/";
-    
-?>
--- php LOGIN FORM/dbcon.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require__DIR__.'/vendor/autoload.php';
-
-use Kreait\Firebase\Factory;
-use Kreait\Firebase\Auth;
-
-$auth = $factory->createAuth();
